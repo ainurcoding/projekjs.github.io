aad97e90c976874662f86dccb477aa6d097727c5
ainurcoding <ainurridwan98@gmail.com>
2020-05-25 23:38:30 +0700

    ainur projek

---
+++ precache-manifest.87296c721d56f650f9f52ec77db2cc02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f342d5e790e348053239c7d5d82f77d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03595ce062d0ba7f6b2c",
+    "url": "/static/css/main.6d2f605a.chunk.css"
+  },
+  {
+    "revision": "5f51c9e2419f13a7d92d",
+    "url": "/static/js/2.5fbed608.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5fbed608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03595ce062d0ba7f6b2c",
+    "url": "/static/js/main.c1a2080e.chunk.js"
+  },
+  {
+    "revision": "2f3923a18d55adb7ecae",
+    "url": "/static/js/runtime-main.44faa172.js"
+  },
+  {
+    "revision": "b8ff65050c8f870ccf15e353e6a13559",
+    "url": "/static/media/food-header.b8ff6505.jpg"
+  }
+]);
